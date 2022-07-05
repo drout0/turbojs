0b51d15e2eb1dc5bb3a881a1c36ffa86ac11822f
drout0 <lawrencebr@hotmail.com>
2022-07-05 20:30:40 -0300

    Version of Turbojs with more than 1 input array
    also the following two lines were inverted, preventing the use of more than one texture (ie input array)
      gl.activeTexture(gl.TEXTURE0); 
      gl.bindTexture(gl.TEXTURE_2D, texture0);

--- turbo.js
+++ turbo.js
@@ -20,9 +20,9 @@
 	"use strict";
 
 	// Mozilla reference init implementation
-	var initGLFromCanvas = function(canvas) {
-		var gl = null;
-		var attr = {alpha : false, antialias : false};
+	const initGLFromCanvas = function(canvas) {
+		let gl = null;
+		const attr = {alpha : false, antialias : false};
 
 		// Try to grab the standard context. If it fails, fallback to experimental.
 		gl = canvas.getContext("webgl", attr) || canvas.getContext("experimental-webgl", attr);
@@ -34,7 +34,7 @@
 		return gl;
 	}
 
-	var gl = initGLFromCanvas(document.createElement('canvas'));
+	const gl = initGLFromCanvas(document.createElement('canvas'));
 
 	// turbo.js requires a 32bit float vec4 texture. Some systems only provide 8bit/float
 	// textures. A workaround is being created, but turbo.js shouldn't be used on those
@@ -44,7 +44,7 @@
 
 	// GPU texture buffer from JS typed array
 	function newBuffer(data, f, e) {
-		var buf = gl.createBuffer();
+		const buf = gl.createBuffer();
 
 		gl.bindBuffer((e || gl.ARRAY_BUFFER), buf);
 		gl.bufferData((e || gl.ARRAY_BUFFER), new (f || Float32Array)(data), gl.STATIC_DRAW);
@@ -52,38 +52,41 @@
 		return buf;
 	}
 
-	var positionBuffer = newBuffer([ -1, -1, 1, -1, 1, 1, -1, 1 ]);
-	var textureBuffer  = newBuffer([  0,  0, 1,  0, 1, 1,  0, 1 ]);
-	var indexBuffer    = newBuffer([  1,  2, 0,  3, 0, 2 ], Uint16Array, gl.ELEMENT_ARRAY_BUFFER);
+	const positionBuffer = newBuffer([ -1, -1, 1, -1, 1, 1, -1, 1 ]);
+	const textureBuffer  = newBuffer([  0,  0, 1,  0, 1, 1,  0, 1 ]);
+	const indexBuffer    = newBuffer([  1,  2, 0,  3, 0, 2 ], Uint16Array, gl.ELEMENT_ARRAY_BUFFER);
 
-	var vertexShaderCode =
-	"attribute vec2 position;\n" +
-	"varying vec2 pos;\n" +
-	"attribute vec2 texture;\n" +
-	"\n" +
-	"void main(void) {\n" +
-	"  pos = texture;\n" +
-	"  gl_Position = vec4(position.xy, 0.0, 1.0);\n" +
-	"}"
+	const vertexShaderCode = 
+	`
+	varying vec2 pos;
+	attribute vec2 position;
+	attribute vec2 texture;
 
-	var stdlib =
-	"\n" +
-	"precision mediump float;\n" +
-	"uniform sampler2D u_texture;\n" +
-	"varying vec2 pos;\n" +
-	"\n" +
-	"vec4 read(void) {\n" +
-	"  return texture2D(u_texture, pos);\n" +
-	"}\n" +
-	"\n" +
-	"void commit(vec4 val) {\n" +
-	"  gl_FragColor = val;\n" +
-	"}\n" +
-	"\n" +
-	"// user code begins here\n" +
-	"\n"
+	void main(void) {
+	  pos = texture;
+	  gl_Position = vec4(position.xy, 0.0, 1.0);
+	}
+	`;
 
-	var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+	const stdlib =
+	`
+	precision mediump float;
+
+	uniform sampler2D u_texture0;
+	uniform sampler2D u_texture1;
+	uniform sampler2D u_texture2;
+	varying vec2 pos;
+
+	vec4 read0(void) { return texture2D(u_texture0, pos); }
+	vec4 read1(void) { return texture2D(u_texture1, pos); }
+	vec4 read2(void) { return texture2D(u_texture2, pos); }
+	//
+	void commit(vec4 val) { gl_FragColor = val; }
+
+	// user code begins here
+	`;
+
+	const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 
 	gl.shaderSource(vertexShader, vertexShaderCode);
 	gl.compileShader(vertexShader);
@@ -99,7 +102,7 @@
 
 	// Transfer data onto clamped texture and turn off any filtering
 	function createTexture(data, size) {
-		var texture = gl.createTexture();
+		const texture = gl.createTexture();
 
 		gl.bindTexture(gl.TEXTURE_2D, texture);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
@@ -114,8 +117,8 @@
 
 	return {
 		// run code against a pre-allocated array
-		run : function(ipt, code) {
-			var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+		run : function(ipt0, ipt1, ipt2, iptRes, code) {
+			const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 
 			gl.shaderSource(
 				fragmentShader,
@@ -127,8 +130,8 @@
 			// Use this output to debug the shader
 			// Keep in mind that WebGL GLSL is **much** stricter than e.g. OpenGL GLSL
 			if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-				var LOC = code.split('\n');
-				var dbgMsg = "ERROR: Could not build shader (fatal).\n\n------------------ KERNEL CODE DUMP ------------------\n"
+				const LOC = code.split('\n');
+				let dbgMsg = "ERROR: Could not build shader (fatal).\n\n------------------ KERNEL CODE DUMP ------------------\n"
 
 				for (var nl = 0; nl < LOC.length; nl++)
 					dbgMsg += (stdlib.split('\n').length + nl) + "> " + LOC[nl] + "\n";
@@ -138,7 +141,7 @@
 				throw new Error(dbgMsg);
 			}
 
-			var program = gl.createProgram();
+			const program = gl.createProgram();
 
 			gl.attachShader(program, vertexShader);
 			gl.attachShader(program, fragmentShader);
@@ -147,44 +150,65 @@
 			if (!gl.getProgramParameter(program, gl.LINK_STATUS))
 				throw new Error('turbojs: Failed to link GLSL program code.');
 
-			var uTexture = gl.getUniformLocation(program, 'u_texture');
-			var aPosition = gl.getAttribLocation(program, 'position');
-			var aTexture = gl.getAttribLocation(program, 'texture');
+			const uTexture0 = gl.getUniformLocation(program, 'u_texture0');
+			const uTexture1 = gl.getUniformLocation(program, 'u_texture1');
+			const uTexture2 = gl.getUniformLocation(program, 'u_texture2');
+			const aPosition = gl.getAttribLocation(program, 'position');
+			const aTexture = gl.getAttribLocation(program, 'texture');
 
 			gl.useProgram(program);
 
-			var size = Math.sqrt(ipt.data.length) / 4;
-			var texture = createTexture(ipt.data, size);
+			const size = Math.sqrt(ipt0.data.length) / 4;
+			const texture0 = createTexture(ipt0.data, size);
+			const texture1 = createTexture(ipt1.data, size);
+			const texture2 = createTexture(ipt2.data, size);
 
 			gl.viewport(0, 0, size, size);
 			gl.bindFramebuffer(gl.FRAMEBUFFER, gl.createFramebuffer());
 
 			// Types arrays speed this up tremendously.
-			var nTexture = createTexture(new Float32Array(ipt.data.length), size);
+			const nTexture = createTexture(new Float32Array(ipt0.data.length), size);
 
 			gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, nTexture, 0);
 
 			// Test for mobile bug MDN->WebGL_best_practices, bullet 7
-			var frameBufferStatus = (gl.checkFramebufferStatus(gl.FRAMEBUFFER) == gl.FRAMEBUFFER_COMPLETE);
+			const frameBufferStatus = (gl.checkFramebufferStatus(gl.FRAMEBUFFER) == gl.FRAMEBUFFER_COMPLETE);
 
 			if (!frameBufferStatus)
 				throw new Error('turbojs: Error attaching float texture to framebuffer. Your device is probably incompatible. Error info: ' + frameBufferStatus.message);
 
-			gl.bindTexture(gl.TEXTURE_2D, texture);
-			gl.activeTexture(gl.TEXTURE0);
-			gl.uniform1i(uTexture, 0);
+			
+			// **************************************************************************************************************************
+			//   ORIGINALLY, the order of these 2 lines (activeTex/bindTex) was inverted, so it wasn't working with more than 1 texture.
+			// **************************************************************************************************************************
+			gl.activeTexture(gl.TEXTURE0); 
+			gl.bindTexture(gl.TEXTURE_2D, texture0);
+			gl.uniform1i(uTexture0, 0);
+			// 
+			gl.activeTexture(gl.TEXTURE1);
+			gl.bindTexture(gl.TEXTURE_2D, texture1);
+			gl.uniform1i(uTexture1, 1);
+			//
+			gl.activeTexture(gl.TEXTURE2);
+			gl.bindTexture(gl.TEXTURE_2D, texture2);
+			gl.uniform1i(uTexture2, 2);
+			//
+			//
 			gl.bindBuffer(gl.ARRAY_BUFFER, textureBuffer);
 			gl.enableVertexAttribArray(aTexture);
 			gl.vertexAttribPointer(aTexture, 2, gl.FLOAT, false, 0, 0);
+			//
 			gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 			gl.enableVertexAttribArray(aPosition);
 			gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+			//
 			gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+			//
 			gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
-			gl.readPixels(0, 0, size, size, gl.RGBA, gl.FLOAT, ipt.data);
+			gl.readPixels(0, 0, size, size, gl.RGBA, gl.FLOAT, iptRes.data);
 			//                                 ^ 4 x 32 bit ^
 
-			return ipt.data.subarray(0, ipt.length);
+			//return ipt0.data.subarray(0, ipt0.length);
 		},
 		alloc: function(sz) {
 			// A sane limit for most GPUs out there.
@@ -192,7 +216,7 @@
 			if (sz > 16777216)
 				throw new Error("turbojs: Whoops, the maximum array size is exceeded!");
 
-			var ns = Math.pow(Math.pow(2, Math.ceil(Math.log(sz) / 1.386) - 1), 2);
+			const ns = Math.pow(Math.pow(2, Math.ceil(Math.log(sz) / 1.386) - 1), 2);
 			return {
 				data : new Float32Array(ns * 16),
 				length : sz
@@ -201,4 +225,5 @@
 	};
 
 }));
+
 
